d37199cf0ae36b9ee6e445ddea82bc5ab35e5b12
Austin Kennedy <kennedyaustin06561@gmail.com>
2020-05-08 17:42:19 -0400

    tried to figure out how to stop the site from adding buttons for empty searches, but could not get it to work, might come back to it later, but started ajax call and have the resulting objects in a console log

--- assets/java.js
+++ assets/java.js
@@ -11,11 +11,51 @@ $(document).ready(function () {
     // This for loop will create a button for the chosen topics above for the user to click on before they add their own
     for (i = 0; i < topics.length; i++) {
 
-      button = $('<button type=' + 'button' + '>' + topics[i] + '</button>').addClass('btn btn-warning').attr('data',topics[i]);
-      $("#buttonBois").append(button);
+      // The buttons will be given a type of 'button', have the content of the index of topics,
+      // be given a class of .btn(for future use), and be assigned a value of the topic it 
+      // corresponds to with the attribute 'data
+      button= $('<button type=' + 'button' + '>' + topics[i] + '</button>').addClass('btn').attr('data', topics[i]);
+      $('#buttonBois').append(button);
 
     };
 
   }
 
-}
+  // Putting this here will make sure that the topics buttons will show up on the page first
+  gifButtons();
+
+  $('#submitboi').on('click', function(event){
+
+    // This will prevent the default behavior of the form since the button is inside of a form
+    event.preventDefault();
+  
+    // Returns the value of the value in the topic input id
+    newGif= $('#gif-input').val();
+    // These two lines will add the new gif to the buttons area
+    topics.push(newGif);
+    gifButtons();
+
+  });
+  
+  // This next part is the ajax call to load the gifs onto the page, and the event listener will be the buttons
+  $('#buttonBois').on('click', '.btn', function() {
+
+    // This pulls the value of the button clicked by the user
+    let gifs= $(this).attr('data')
+    var queryURL= 'https://api.giphy.com/v1/gifs/search?q=' + gifs + '&api_key=pt60h37z2e8OWaFPknx60JqEPkdjokFx'
+
+    $.ajax({
+
+      url: queryURL,
+      method: 'GET'
+
+    }).then(function(response) {
+
+      console.log(response)
+      let results= response.data;
+
+    })
+
+  })
+
+})
